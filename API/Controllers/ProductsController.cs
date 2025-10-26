cd2f5974f6019091979d808c7af5de854028bf76
vladan77 <vladan77milica@gmail.com>
2025-10-26 22:44:29 +0100

    Section 6-1

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,14 +1,11 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController(StoreContext context) : ControllerBase
+    public class ProductsController(StoreContext context) : BaseApiController
     {
 
         [HttpGet]
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.ExceptionServices;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware(IHostEnvironment env, ILogger<ExceptionMiddleware> logger) : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleException(context, ex);
+        }
+    }
+
+    private async Task HandleException(HttpContext context, Exception ex)
+    {
+        throw new NotImplementedException();
+    }
+}
