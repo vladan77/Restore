d5a7b9e2dcd28dfd49889e870a3fbee1698cf366
vladan77 <vladan77milica@gmail.com>
2026-02-22 14:19:05 +0100

    Secton 11 - PaymentController

--- API/Controllers/PaymentController.cs
+++ API/Controllers/PaymentController.cs
@@ -2,15 +2,11 @@
 using System;
 using API.Data;
 using API.DTOs;
-<<<<<<< HEAD
 using API.Entities.OrderAggregate;
-=======
->>>>>>> 13bdc0858edce8ee09b20d1253cf3f6628287587
 using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
 using Microsoft.EntityFrameworkCore;
 using Stripe;
 
@@ -18,12 +14,6 @@ namespace API.Controllers;
 
 public class PaymentsController(PaymentsService paymentsService, 
 StoreContext context, IConfiguration config, ILogger<PaymentsController> logger) : BaseApiController
-=======
-
-namespace API.Controllers;
-
-public class PaymentsController(PaymentsService paymentsService, StoreContext context) : BaseApiController
->>>>>>> 13bdc0858edce8ee09b20d1253cf3f6628287587
 {
     [Authorize]
     [HttpPost]
